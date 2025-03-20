4e2fb8d203766a5c92486391bf80ef4c95cee530
farhan-20574 <farhan20574@outlook.com>
2025-03-20 11:37:16 +0500

    middle of class

--- out/index.js
+++ out/index.js
@@ -1,2 +1,22 @@
 "use strict";
-let fullname = "Hellow";
+//  const processData = (input: string | number[]): any => {
+//   if (typeof input === 'string') {
+//     return input.toLocaleLowerCase();
+//   }else {
+//     typeof input === 'isarray(input)';
+//   }
+//   processData ("HLLO");
+const processData = (input) => {
+    if (typeof input === 'string') {
+        return input.toLocaleLowerCase();
+    }
+    else {
+        let sum = 0;
+        for (const element of input) {
+            sum += element;
+        }
+        return sum;
+    }
+};
+console.log(processData("HELLO"));
+console.log(processData([1, 2, 3, 4]));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-let fullname = "Hellow"
+//  const processData = (input: string | number[]): any => {
+//   if (typeof input === 'string') {
+//     return input.toLocaleLowerCase();
+//   }else {
+//     typeof input === 'isarray(input)';
+//   }
+//   processData ("HLLO");
 
+interface processDataType {
+    (input: string | number[]): any;
+}
+
+const processData: processDataType = (input) => {
+    if (typeof input === 'string') {
+        return input.toLocaleLowerCase();
+    } 
+ else {
+        let sum = 0;
+        for (const element of input) {
+            sum += element;
+        }
+        return sum;
+    }
+    
+}
+
+console.log(processData("HELLO"));
+console.log(processData([1, 2, 3, 4]));
